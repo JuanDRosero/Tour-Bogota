4a2e155961c8259738bda7a1592b27e06a52ec54
Juan David <jdrosero59@gmail.com>
2021-05-27 17:49:18 -0500

    Activity 1 y 2

---
+++ app/src/main/java/com/example/jdros/tourbogota/menuRutas.java
@@ -0,0 +1,16 @@
+package com.example.jdros.tourbogota;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class menuRutas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu__rutas);
+
+        getSupportActionBar().hide();
+    }
+}
